99863fc9b09dc69988d9e2608d8c1838af3841b1
maryam.rasoolizadeh <maryrasooli3@gmail.com>
2024-11-17 17:34:52 +0330

    urls files added to apps and kernel urls updated

--- kernel/urls.py
+++ kernel/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/',include('account.urls')),
+    path('sport/',include('sport.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
