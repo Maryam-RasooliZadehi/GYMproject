bef725e877a7721292ff0d11edfd7da44b734c2e
Miamaghani <kimiamaghanii@gmail.com>
2024-11-18 22:24:53 +0330

    user api view class get and put

--- account/api/v1/views.py
+++ account/api/v1/views.py
@@ -15,3 +15,19 @@ class RegistrationApiView(generics.GenericAPIView):
         data = copy(serlizer.data)
         data.pop("password", None)
         return Response(data, status= status.HTTP_201_CREATED)
+    
+
+class UserAPIView(generics.GenericAPIView):
+    serializer_class = UpdateUserAPIViewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self,request):
+        user = request.user
+        serializer = UserAPIViewSerializer(user)
+        serializer.data['eror'] = False
+        return Response(serializer.data)
+    
+    def put(self,request):
+        serializer = self.serializer_class(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        User.objects.filter(id = request.user.id).update(**serializer.validated_data)
+        return Response({"error":False , "detail":"user updated successfully!"})
