35000a58485cae657d9aff6217a2285a14bda281
maryam.rasoolizadeh <maryrasooli3@gmail.com>
2024-11-20 23:06:23 +0330

    name of serializer edited

---
+++ sport/api/v1/views.py
@@ -0,0 +1,14 @@
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import status , permissions ,generics
+from rest_framework.pagination import PageNumberPagination
+from ...models import Course , Action , Diet , Plan
+from django.db.models import Q
+from .serializers import CourseSerializer , ActionSerializer , DietSerializer , CreateDietSerializer , UpdateDietSerializer
+from .serializers import PlanSerializer , CreatePlanSerializer , UpdatePlanSerializer
+from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .filters import PlanFilter, DietFilter 
+User = get_user_model()
+
