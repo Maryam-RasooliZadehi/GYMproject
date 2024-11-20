fe86b586bcc8636c2712d225805996de1ca7d780
maryam.rasoolizadeh <maryrasooli3@gmail.com>
2024-11-20 21:32:22 +0330

    create and update plan serializer

--- sport/api/v1/serializeers.py
+++ sport/api/v1/serializeers.py
@@ -5,7 +5,6 @@ from account.api.v1.serializers import UserAPIViewSerializer
 class CourseSerializer(serializers.ModelSerializer):
     teacher = UserAPIViewSerializer()
     student = UserAPIViewSerializer()
-
     class Meta:
         model = Course
         fields = "__all__"
@@ -16,13 +15,22 @@ class ActionSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class DietSerializer(serializers.ModelSerializer):
+    course = CourseSerializer()
+    class Meta:
+        model = Diet
+        fields = '__all__'
+
+
+class CreateDietSerializer(serializers.ModelSerializer):
     class Meta:
         exclude = ["created_date", "updated_date"]
     #checks to see if the user who requests to make a diet is the teacher of the course or not.
+
     def validate(self, attrs):
         course = attrs.get('course')
         if course.teacher != self.context['request'].user:
-            raise serializers.ValidationError({"detail":"you are noit the teacher of this course."})
+            raise serializers.ValidationError({"detail":"you are not the teacher of this course."})
+
         return super().validate(attrs)
     
 class UpdateDietSerializer(serializers.ModelSerializer):
@@ -33,10 +41,34 @@ class UpdateDietSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         if self.instance.course.teacher != self.context["request"].user:
             raise serializers.ValidationError({"detail":"you are not the teacher of the course to edit this diet."})
+
         return super().validate(attrs)
     
 class PlanSerializer(serializers.ModelSerializer):
     class meta:
         fields = "__all__"
 
+
+class CreatePlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        exclude = ["created_date","updated_date"]
+
+            #checks to see if the user who requests to write a plan is the teacher of the course or not.
+        def validate(self, attrs):
+            course = attrs.get('course')
+            if course.teacher != self.context['request'].user:
+                raise serializers.ValidationError({"detail":"you are not the teacher of this course."})
+            return super().validate(attrs)
         
+
+class UpdatePlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ["action","set_number","number_per_set","start_date","end_date","day_of_week"]
+
+        def validate(self, attrs):
+            if self.instance.course.teacher != self.context["request"].user:
+                raise serializers.ValidationError({"detail":"you are not the teacher of this course"})
+
+            return super().validate(attrs)         
