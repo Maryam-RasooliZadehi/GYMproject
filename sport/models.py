01d3f2fca84b7b57a50f9dc35386fa954d8a0795
maryam.rasoolizadeh <maryrasooli3@gmail.com>
2024-11-20 18:12:23 +0330

    Course model

--- sport/models.py
+++ sport/models.py
@@ -13,6 +13,27 @@ class Action(models.Model):
     def __str__ (self):
         return self.title
     
+class Course(models.Model):
+    teacher = models.ForeignKey(User,on_delete=models.CASCADE,related_name="course_teacher")
+    student = models.ForeignKey(User,on_delete=models.CASCADE,related_name="course_student")
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    
+    def save(self , *args , **kwargs):
+         # Ensures that teacher has the user_type "teacher"
+         if self.teacher.user_type != 'teacher':
+            raise ValidationError({'teacher':'selected user must have user_type "teacher"'})
+         
+         # Ensures that student has the user_type "student"
+         if self.teacher.user_type != 'student':
+            raise ValidationError({'student':'selected user must have user_type "student"'})
+         
+         super().save(*args , **kwargs)
+
+
+         
+         
 
 
 
