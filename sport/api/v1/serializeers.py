1184f775a77121507a4c95c1b3c57932f076f403
maryam.rasoolizadeh <maryrasooli3@gmail.com>
2024-11-20 20:23:07 +0330

    Diet serializer

--- sport/api/v1/serializeers.py
+++ sport/api/v1/serializeers.py
@@ -14,3 +14,13 @@ class ActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Action
         fields = "__all__"
+
+class DietSerializer(serializers.ModelSerializer):
+    class Meta:
+        exclude = ["created_date", "updated_date"]
+    #cheecks to see if the user who requests to make a diet is the teacher of the course or not.
+    def validate(self, attrs):
+        course = attrs.get('course')
+        if course.teacher != self.context['request'].user:
+            raise serializers.ValidationError({"detail":"you are noit the teacher of this course."})
+        return super().validate(attrs)
