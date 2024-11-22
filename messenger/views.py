a163a8b1eefc215f8dfd55e5583c68eff5cc74dd
Miamaghani <kimiamaghanii@gmail.com>
2024-11-22 17:34:44 +0330

    Add custom pagination with unread conversations count

--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.response import Response
+from .models import Conversation
+from django.contrib.auth import get_user_model
+from rest_framework.pagination import PageNumberPagination
+
+User = get_user_model()
+
+
+class ListPagination(PageNumberPagination):
+    page_size = 10 
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'new_conversations': Conversation.objects.filter(conversationmessage__viewed=False).exclude(conversationmessage__user=self.request.user.id).distinct().count(),
+            'results': data,
+        })
